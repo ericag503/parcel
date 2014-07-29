29da9404977bcf06098fa820d69c6b9a76cb01c4
Erica Gordon & Aoede Pando <student@epicodus.com>
2014-07-29 11:47:53 -0700

    third commit, claculates the cost to ship by mulitplying volume and cost

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -1,19 +1,21 @@
-
-
 class Parcel
   def initialize(length, width, height, weight)
     @length = length
     @width = width
     @height = height
     @weight = weight
-    # @description = description
+    @volume = volume
   end
 
   def volume
     @length * @width * @height
   end
 
+  def cost
+    @weight + @volume
+  end
 end
 
 Parcel.new(5, 4, 6, 120)
+
 
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -3,11 +3,15 @@ require 'parcel'
 
 describe Parcel do
   it 'creates new parcel with specified dimensions and weight' do
-    test_parcel = Parcel.new(5, 4, 6, 120)
-      test_parcel.should be_an_instance_of Parcel
+    test_parcel = Parcel.new(5, 4, 6, 100)
+    test_parcel.should be_an_instance_of Parcel
   end
   it 'calculates the volume of a new parcel' do
-    test_parcel = Parcel.new(5, 4, 6, 120)
+    test_parcel = Parcel.new(5, 4, 6, 100)
     test_parcel.volume.should eq 120
+  end
+  it 'calculates the cost to ship by multiplying volume and cost' do
+    test_parcel = Parcel.new(5, 4, 6, 100)
+    test_parcel.cost.should eq 220
   end
 end
