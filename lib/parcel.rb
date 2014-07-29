a09acde11841c88653a4af5027a3c5ec84b1dc6a
Erica Gordon & Aoede Pando <student@epicodus.com>
2014-07-29 10:31:25 -0700

    initial commit creates new parcel with specified dimensions and weight

---
+++ lib/parcel.rb
@@ -0,0 +1,28 @@
+class Parcel
+  def initialize(length, width, weight)
+    @length = length
+    @width = width
+    @weight = weight
+    # @description = description
+  end
+
+  # def length
+  #   @length
+  # end
+
+  # def width
+  #   @width
+  # end
+
+  # def weight
+  #   @weight
+  # end
+
+  # def descritption
+  #   @description
+  # end
+
+end
+
+
+
