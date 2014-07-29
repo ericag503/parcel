7f687e83702226c9a8af99fa05ec0068cdf9dd37
Erica Gordon & Aoede Pando <student@epicodus.com>
2014-07-29 10:50:15 -0700

    second commit, calculates volume of a new parcel

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -1,28 +1,19 @@
+
+
 class Parcel
-  def initialize(length, width, weight)
+  def initialize(length, width, height, weight)
     @length = length
     @width = width
+    @height = height
     @weight = weight
     # @description = description
   end
 
-  # def length
-  #   @length
-  # end
-
-  # def width
-  #   @width
-  # end
-
-  # def weight
-  #   @weight
-  # end
-
-  # def descritption
-  #   @description
-  # end
+  def volume
+    @length * @width * @height
+  end
 
 end
 
-
+Parcel.new(5, 4, 6, 120)
 
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -3,9 +3,11 @@ require 'parcel'
 
 describe Parcel do
   it 'creates new parcel with specified dimensions and weight' do
-    test_parcel = Parcel.new(5, 4, 120)
+    test_parcel = Parcel.new(5, 4, 6, 120)
       test_parcel.should be_an_instance_of Parcel
   end
-  # it 'calculates the volume of a new parcel' do
-
+  it 'calculates the volume of a new parcel' do
+    test_parcel = Parcel.new(5, 4, 6, 120)
+    test_parcel.volume.should eq 120
+  end
 end
